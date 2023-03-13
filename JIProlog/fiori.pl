56041fccc8aa15cf3d13c8f3c9ce75005129569d
Pierre Paul <pierrepaul.charbonnier@gmail.com>
2023-03-13 20:00:08 +0100

    exemple of Jiprolog + pom has been updated with lib

---
+++ JIProlog/fiori.pl
@@ -0,0 +1,9 @@
+fiore(f1).
+fiore(f2).
+fiore(f3).
+fiore(f4).
+
+colore(f1,rosso). 
+colore(f2,giallo).
+colore(f3,blu).
+colore(f4,verde).
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module chatbot.project22 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires jiprolog;
+    requires java.desktop;
 
     opens chatbot.project22.GUI to javafx.fxml;
     exports chatbot.project22.GUI;
