7a750314ca74866d234f3911dd85fd0094dee568
PierrePaul Charbonnier <pierrepaul.charbonnier@gmail.com>
2023-03-14 18:10:50 +0100

    fixed maven deps - added JProlog Source files -removed module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module chatbot.project22 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jiprolog;
-    requires java.desktop;
-
-    opens chatbot.project22.GUI to javafx.fxml;
-    exports chatbot.project22.GUI;
-}
