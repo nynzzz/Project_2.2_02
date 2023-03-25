116000aa4f36df3b311b7912e45221e950c9f388
zijiandongkurt <57815948+zijiandongkurt@users.noreply.github.com>
2023-03-25 18:01:05 +0100

    YAML agent files

---
+++ agent/src/main/java/project/multi/agent/Symbol.java
@@ -0,0 +1,29 @@
+package project.multi.agent;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Symbol {
+	String name;
+	List<List<Symbol>> productions;
+
+	Symbol(String name, List<List<Symbol>> productions) {
+		this.name = name;
+		this.productions = productions;
+	}
+
+	public Symbol(String name) {
+		this(name, new java.util.ArrayList<>());
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	
+	 
+	public String toStringLong() {
+		return name + Arrays.toString(productions.toArray());
+	}
+}
