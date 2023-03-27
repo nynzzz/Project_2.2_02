09186514c81edded35ca8e73edfa1fe11df1b5f9
Pierre Paul Charbonnier <ppcharbo>
2023-03-27 21:13:43 +0200

    added rule engine

---
+++ src/main/java/project/multi/agent/Token.java
@@ -0,0 +1,45 @@
+package project.multi.agent;
+
+import java.util.Objects;
+
+public class Token {
+    private final TokenType type;
+    private final String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return type + ": " + lexeme;
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(lexeme);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		return Objects.equals(lexeme, other.lexeme);
+	}
+    
+    
+}
---
+++ src/main/java/project/multi/agent/TokenType.java
@@ -0,0 +1,18 @@
+package project.multi.agent;
+
+public enum TokenType {
+    // Keywords
+    S,
+    ACTION,
+    SCHEDULE,
+    TIMEEXPRESSION,
+    TIME,
+    LOCATION,
+    PRO,
+    ROOM,
+    DAY,
+
+    // Other tokens
+    ERROR,
+    EOF
+}
